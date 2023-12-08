f6fd8d34eb2842407d893ba0ec5718d3ae05cfbf
Dmitriy Gaydukov <gaydukov89@gmail.com>
2023-12-09 00:05:22 +0400

    move matching-engine snpashot test into separate file

--- src/test/java/com/exchange/core/integration/MatchingEngineSnapshotTest.java
+++ src/test/java/com/exchange/core/integration/MatchingEngineSnapshotTest.java
@@ -7,7 +7,14 @@ import com.exchange.core.model.SnapshotItem;
 import com.exchange.core.model.enums.OrderSide;
 import com.exchange.core.model.enums.OrderStatus;
 import com.exchange.core.model.enums.SnapshotType;
-import com.exchange.core.model.msg.*;
+import com.exchange.core.model.msg.ErrorMessage;
+import com.exchange.core.model.msg.ExecutionReport;
+import com.exchange.core.model.msg.InstrumentConfig;
+import com.exchange.core.model.msg.MarketData;
+import com.exchange.core.model.msg.Message;
+import com.exchange.core.model.msg.Order;
+import com.exchange.core.model.msg.SnapshotMessage;
+import com.exchange.core.model.msg.UserBalance;
 import com.exchange.core.user.Account;
 import com.exchange.core.user.Position;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -19,176 +26,16 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-import java.util.LinkedList;
-import java.util.Queue;
-import org.mockito.BDDMockito.Then;
+public class MatchingEngineSnapshotTest {
 
-public class MatchingEngineIntegrationTest {
   private final String SNAPSHOT_BASE_DIR = System.getProperty("user.dir") + "/snapshots";
-
-  @Test
-  public void invalidSymbolTest() {
-    Queue<Message> inbound = new LinkedList<>();
-    Queue<Message> outbound = new LinkedList<>();
-    MatchingEngine me = new MatchingEngine(inbound, outbound);
-    Assertions.assertNull(outbound.poll());
-    me.start();
-    Assertions.assertNull(outbound.poll());
-
-    Message msg;
-
-    InstrumentConfig inst = MockData.getInstrument();
-    Order buy = MockData.getLimitBuy();
-    buy.setSymbol(null);
-
-    inbound.add(buy);
-    sleep();
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ErrorMessage.class, msg);
-    ErrorMessage invalidSymbolError = (ErrorMessage) msg;
-    Assertions.assertEquals("Symbol of new order can't be null", invalidSymbolError.getError(),
-        "error text mismatch");
-
-    buy.setSymbol("ABC");
-    inbound.add(buy);
-    sleep();
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ErrorMessage.class, msg);
-    ErrorMessage orderBookNotFound = (ErrorMessage) msg;
-    Assertions.assertEquals("OrderBook not found for symbol=ABC", orderBookNotFound.getError(),
-        "error text mismatch");
-  }
-
-  @Test
-  public void preOrderCheckFailTest() {
-    Queue<Message> inbound = new LinkedList<>();
-    Queue<Message> outbound = new LinkedList<>();
-    MatchingEngine me = new MatchingEngine(inbound, outbound);
-    me.start();
-
-    InstrumentConfig inst = MockData.getInstrument();
-    inbound.add(inst);
-    Order order = MockData.getLimitBuy();
-    inbound.add(order);
-    sleep();
-    Message msg;
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ErrorMessage.class, msg);
-    ErrorMessage accountNotFoundError = (ErrorMessage) msg;
-    Assertions.assertEquals("Account not found", accountNotFoundError.getError(),
-        "error text mismatch");
-
-    UserBalance user = MockData.getUser(inst.getQuote());
-    user.setAmount(BigDecimal.ZERO);
-    inbound.add(user);
-    inbound.add(order);
-    sleep();
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ErrorMessage.class, msg);
-    ErrorMessage notEnoughBalance = (ErrorMessage) msg;
-    Assertions.assertEquals("Balance insufficient", notEnoughBalance.getError(),
-        "error text mismatch");
-  }
-
-  @Test
-  public void placeAndMatchOrderTest() {
-    Queue<Message> inbound = new LinkedList<>();
-    Queue<Message> outbound = new LinkedList<>();
-    MatchingEngine me = new MatchingEngine(inbound, outbound);
-    me.start();
-    // add instrument config
-    InstrumentConfig inst = MockData.getInstrument();
-    inbound.add(inst);
-    // add balance
-    UserBalance user = MockData.getUser(inst.getQuote());
-    inbound.add(user);
-    // add order
-    Order buy = MockData.getLimitBuy();
-    inbound.add(buy);
-
-    sleep();
-    Message msg;
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ExecutionReport.class, msg);
-    ExecutionReport exec = (ExecutionReport) msg;
-    Assertions.assertEquals(1, exec.getOrderId(), "orderId mismatch");
-    Assertions.assertEquals(1, exec.getExecId(), "execId mismatch");
-    Assertions.assertEquals(buy.getSymbol(), exec.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(buy.getOrderQty(), exec.getOrderQty(), "orderQty mismatch");
-    Assertions.assertEquals(buy.getPrice(), exec.getPrice(), "price mismatch");
-    Assertions.assertEquals(OrderStatus.NEW, exec.getStatus(), "status should be new");
-
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(MarketData.class, msg);
-    MarketData md = (MarketData) msg;
-    Assertions.assertEquals(buy.getSymbol(), md.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(1, md.getDepth(), "depth mismatch");
-    Assertions.assertEquals(1, md.getBids().length, "should be 1 bids");
-    BigDecimal[][] bids = new BigDecimal[][]{
-        {new BigDecimal("100"), new BigDecimal("10")}
-    };
-    Assertions.assertArrayEquals(bids, md.getBids(), "bids mismatch");
-    Assertions.assertEquals(0, md.getAsks().length, "should be 0 asks");
-
-    // add sell user balance
-    final int sellUserId = 2;
-    UserBalance sellUser = MockData.getUser(inst.getBase());
-    sellUser.setAmount(new BigDecimal("20"));
-    sellUser.setAccount(sellUserId);
-    inbound.add(sellUser);
-    // add order
-    Order sell = MockData.getLimitBuy();
-    sell.setSide(OrderSide.SELL);
-    sell.setOrderQty(new BigDecimal("15"));
-    sell.setAccount(sellUserId);
-    inbound.add(sell);
-
-    sleep();
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ExecutionReport.class, msg);
-    ExecutionReport takerExec = (ExecutionReport) msg;
-    Assertions.assertEquals(2, takerExec.getOrderId(), "orderId mismatch");
-    Assertions.assertEquals(2, takerExec.getExecId(), "execId mismatch");
-    Assertions.assertEquals(sell.getSymbol(), takerExec.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(sell.getOrderQty(), takerExec.getOrderQty(), "orderQty mismatch");
-    Assertions.assertEquals(sell.getPrice(), takerExec.getPrice(), "price mismatch");
-    Assertions.assertEquals(OrderStatus.NEW, takerExec.getStatus(), "status should be new");
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ExecutionReport.class, msg);
-    ExecutionReport takerTrade = (ExecutionReport) msg;
-    Assertions.assertEquals(2, takerTrade.getOrderId(), "orderId mismatch");
-    Assertions.assertEquals(3, takerTrade.getExecId(), "execId mismatch");
-    Assertions.assertEquals(sell.getSymbol(), takerTrade.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(sell.getOrderQty(), takerTrade.getOrderQty(), "orderQty mismatch");
-    Assertions.assertEquals(sell.getPrice(), takerTrade.getPrice(), "price mismatch");
-    Assertions.assertEquals(OrderStatus.PARTIALLY_FILLED, takerTrade.getStatus(),
-        "status should be partially filled");
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(ExecutionReport.class, msg);
-    ExecutionReport makerTrade = (ExecutionReport) msg;
-    Assertions.assertEquals(1, makerTrade.getOrderId(), "orderId mismatch");
-    Assertions.assertEquals(4, makerTrade.getExecId(), "execId mismatch");
-    Assertions.assertEquals(buy.getSymbol(), makerTrade.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(buy.getOrderQty(), makerTrade.getOrderQty(), "orderQty mismatch");
-    Assertions.assertEquals(buy.getPrice(), makerTrade.getPrice(), "price mismatch");
-    Assertions.assertEquals(OrderStatus.FILLED, makerTrade.getStatus(), "status should be filled");
-    msg = outbound.poll();
-    Assertions.assertInstanceOf(MarketData.class, msg);
-    MarketData md2 = (MarketData) msg;
-    Assertions.assertEquals(buy.getSymbol(), md2.getSymbol(), "symbol mismatch");
-    Assertions.assertEquals(1, md2.getDepth(), "depth mismatch");
-    Assertions.assertEquals(0, md2.getBids().length, "should be 0 bids");
-    Assertions.assertEquals(1, md2.getAsks().length, "should be 1 asks");
-    BigDecimal[][] asks = new BigDecimal[][]{
-        {new BigDecimal("100"), new BigDecimal("5")}
-    };
-    Assertions.assertArrayEquals(asks, md2.getAsks(), "asks mismatch");
-  }
 
   @Test
   public void makeSnapshotTest() throws InterruptedException, IOException {
@@ -230,7 +77,8 @@ public class MatchingEngineIntegrationTest {
     reader.close();
     final ObjectMapper mapper = new ObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    List<SnapshotItem> snapshotItems = mapper.readValue(content, new TypeReference<>() {});
+    List<SnapshotItem> snapshotItems = mapper.readValue(content, new TypeReference<>() {
+    });
     Assertions.assertEquals(3, snapshotItems.size(), "should be 3 items in the snapshot");
     // validate instrument
     Object instrumentData = snapshotItems
@@ -240,7 +88,9 @@ public class MatchingEngineIntegrationTest {
         .findFirst()
         .orElse(null);
     Assertions.assertNotNull(instrumentData);
-    List<InstrumentConfig> instruments = mapper.readValue(mapper.writeValueAsString(instrumentData), new TypeReference<>() {});
+    List<InstrumentConfig> instruments = mapper.readValue(mapper.writeValueAsString(instrumentData),
+        new TypeReference<>() {
+        });
     Assertions.assertEquals(1, instruments.size(), "should be 1 instrument");
     Assertions.assertEquals(inst, instruments.get(0), "instrument mismatch");
     // validate account
@@ -251,7 +101,9 @@ public class MatchingEngineIntegrationTest {
         .findFirst()
         .orElse(null);
     Assertions.assertNotNull(accountData);
-    List<Account> accounts = mapper.readValue(mapper.writeValueAsString(accountData), new TypeReference<>() {});
+    List<Account> accounts = mapper.readValue(mapper.writeValueAsString(accountData),
+        new TypeReference<>() {
+        });
     Assertions.assertEquals(2, accounts.size(), "should be 2 account");
     Account account = accounts
         .stream()
@@ -273,7 +125,9 @@ public class MatchingEngineIntegrationTest {
         .findFirst()
         .orElse(null);
     Assertions.assertNotNull(orderBookData);
-    List<Order> orders = mapper.readValue(mapper.writeValueAsString(orderBookData), new TypeReference<>() {});
+    List<Order> orders = mapper.readValue(mapper.writeValueAsString(orderBookData),
+        new TypeReference<>() {
+        });
     Assertions.assertEquals(1, orders.size(), "should be 1 order");
     Order order = orders.get(0);
     Assertions.assertEquals(buy, order, "order mismatch");
@@ -322,7 +176,8 @@ public class MatchingEngineIntegrationTest {
     Assertions.assertEquals(sell.getOrderQty(), takerFilled.getOrderQty(), "orderQty mismatch");
     Assertions.assertEquals(sellLeavesQty, takerFilled.getLeavesQty(), "leavesQty mismatch");
     Assertions.assertEquals(sell.getPrice(), takerFilled.getPrice(), "price mismatch");
-    Assertions.assertEquals(OrderStatus.PARTIALLY_FILLED, takerFilled.getStatus(), "status should be partially_filled");
+    Assertions.assertEquals(OrderStatus.PARTIALLY_FILLED, takerFilled.getStatus(),
+        "status should be partially_filled");
 
     ExecutionReport makerFilled = (ExecutionReport) outbound.poll();
     Assertions.assertEquals(makerOrderId, makerFilled.getOrderId(), "orderId mismatch");
@@ -347,13 +202,5 @@ public class MatchingEngineIntegrationTest {
 
     // cleanup
     TestUtils.deleteDirectory(baseDir);
-  }
-
-
-  private void sleep() {
-    try {
-      Thread.sleep(50);
-    } catch (InterruptedException ex) {
-    }
   }
 }
