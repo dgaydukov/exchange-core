e45bee80952bd31fb0259f3456a55350d03935b1
Dmitriy Gaydukov <gaydukov89@gmail.com>
2023-12-13 19:43:49 +0400

    add bigdecimal rounding precision loss test

--- src/test/java/com/exchange/core/PrecisionLossTest.java
+++ src/test/java/com/exchange/core/PrecisionLossTest.java
@@ -1,10 +1,20 @@
 package com.exchange.core;
 
+import com.exchange.core.config.AppConstants;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PrecisionLossTest {
+
   @Test
-  private void bigDecimalTest(){
-    
+  public void bigDecimalTest(){
+    BigDecimal amount = new BigDecimal("111");
+    BigDecimal price = new BigDecimal("219");
+    BigDecimal tradeQty = amount.divide(price, AppConstants.ROUNDING_SCALE, RoundingMode.DOWN);
+    BigDecimal tradeAmount = tradeQty.multiply(price);
+    System.out.println("tradeQty="+tradeQty+", tradeAmount="+tradeAmount);
+    Assertions.assertNotEquals(amount, tradeAmount, "values shouldn't match");
   }
 }
