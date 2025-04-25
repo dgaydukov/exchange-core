a7f7ece68293f670660e954e0f92599d5ee1b9b9
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-04-25 17:56:58 +0300

    add order book based on indexed priority queue

---
+++ src/main/java/com/exchange/core/matching/orderbook/book/IndexedPQOrderBook.java
@@ -0,0 +1,35 @@
+package com.exchange.core.matching.orderbook.book;
+
+import com.exchange.core.matching.orderbook.OrderBook;
+import com.exchange.core.model.Trade;
+import com.exchange.core.model.msg.MarketData;
+import com.exchange.core.model.msg.Order;
+
+import java.util.List;
+
+public class IndexedPQOrderBook implements OrderBook {
+    @Override
+    public List<Trade> match(Order order) {
+        return List.of();
+    }
+
+    @Override
+    public boolean add(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(long orderId) {
+        return false;
+    }
+
+    @Override
+    public MarketData buildMarketData() {
+        return null;
+    }
+}
