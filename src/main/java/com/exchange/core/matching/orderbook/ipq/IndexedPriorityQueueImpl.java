de3f4b2504f7ef62cf6d4205fde2c116fae82d40
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-04-29 13:27:54 +0300

    add map-based implementation for IndexedPQ

--- src/main/java/com/exchange/core/matching/orderbook/ipq/IndexedPriorityQueueImpl.java
+++ src/main/java/com/exchange/core/matching/orderbook/ipq/IndexedPriorityQueueImpl.java
@@ -1,41 +1,60 @@
 package com.exchange.core.matching.orderbook.ipq;
 
+import java.util.Comparator;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class IndexedPriorityQueueImpl<K extends Comparable<K>, V> implements IndexedPriorityQueue<K, V> {
+    private int size;
+    private int capacity;
+    private final int growSize;
+    private final SortOrder sortOrder;
+    private final NavigableMap<K, V> map;
 
-
-    public IndexedPriorityQueueImpl (){
-        this(SortOrder.ASC);
-    }
     public IndexedPriorityQueueImpl (SortOrder sortOrder){
         this(sortOrder, 1000, 100);
     }
     public IndexedPriorityQueueImpl(SortOrder sortOrder, int capacity, int growSize){
+        this.capacity = capacity;
+        this.growSize = growSize;
+        this.sortOrder = sortOrder;
+        Comparator<K> comparator = sortOrder == SortOrder.ASC ? Comparator.naturalOrder() : Comparator.reverseOrder();
+        map = new TreeMap<>(comparator);
     }
 
     @Override
     public void offer(K key, V value) {
-
+        map.put(key, value);
     }
 
     @Override
     public V poll() {
-        return null;
+        if (map.size() == 0){
+            return null;
+        }
+        return map.pollFirstEntry().getValue();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public V getExact(K key) {
-        return null;
+        return map.get(key);
     }
 
     @Override
     public V getNearestLeft(K key) {
-        return null;
+        return map.get(getNearestLeftKey(key));
+    }
+
+    private K getNearestLeftKey(K key){
+        K before = map.floorKey(key);
+        K after = map.ceilingKey(key);
+        if (before == null) return after;
+        if (after == null) return before;
+        return before.compareTo(after) > 0 ? before : after;
     }
 }
