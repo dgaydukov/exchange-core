4de3fd648e01e577897c5ad44012f39e8fa91da3
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-04-30 21:22:33 +0300

    add 2 orderbooks to performance testing

--- src/test/java/performance/OrderBookPerformanceTest.java
+++ src/test/java/performance/OrderBookPerformanceTest.java
@@ -1,28 +1,21 @@
 package performance;
 
 import com.exchange.core.MockData;
-import com.exchange.core.matching.orderbook.book.MapOrderBook;
 import com.exchange.core.matching.orderbook.OrderBook;
 import com.exchange.core.matching.orderbook.book.ArrayOrderBook;
+import com.exchange.core.matching.orderbook.book.IpqOrderBook;
+import com.exchange.core.matching.orderbook.book.LinkedListOrderBook;
+import com.exchange.core.matching.orderbook.book.MapOrderBook;
 import com.exchange.core.model.Trade;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -33,6 +26,8 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 public class OrderBookPerformanceTest {
 
   private OrderBook arrayOrderBook;
+  private OrderBook ipqOrderBook;
+  private OrderBook linkedListOrderBook;
   private OrderBook mapOrderBook;
 
   public static void main(String[] args) throws RunnerException {
@@ -45,16 +40,10 @@ public class OrderBookPerformanceTest {
 
   @Setup(Level.Iteration)
   public void setUp() {
-    mapOrderBook = new MapOrderBook(MockData.SYMBOL);
     arrayOrderBook = new ArrayOrderBook(MockData.SYMBOL);
-  }
-
-  @Benchmark
-  public void measureMapOrderBook(Blackhole blackhole) {
-    mapOrderBook.add(RandomOrder.buyLimitUser1());
-    List<Trade> trades = mapOrderBook.match(RandomOrder.sellLimitUser2());
-    blackhole.consume(trades);
-    blackhole.consume(mapOrderBook.buildMarketData());
+    ipqOrderBook = new IpqOrderBook(MockData.SYMBOL);
+    linkedListOrderBook = new LinkedListOrderBook(MockData.SYMBOL);
+    mapOrderBook = new MapOrderBook(MockData.SYMBOL);
   }
 
   @Benchmark
@@ -63,5 +52,29 @@ public class OrderBookPerformanceTest {
     List<Trade> trades = arrayOrderBook.match(RandomOrder.sellLimitUser2());
     blackhole.consume(trades);
     blackhole.consume(arrayOrderBook.buildMarketData());
+  }
+
+  @Benchmark
+  public void measureIpqOrderBook(Blackhole blackhole) {
+    ipqOrderBook.add(RandomOrder.buyLimitUser1());
+    List<Trade> trades = ipqOrderBook.match(RandomOrder.sellLimitUser2());
+    blackhole.consume(trades);
+    blackhole.consume(ipqOrderBook.buildMarketData());
+  }
+
+  @Benchmark
+  public void measureLinkedListOrderBook(Blackhole blackhole) {
+    linkedListOrderBook.add(RandomOrder.buyLimitUser1());
+    List<Trade> trades = linkedListOrderBook.match(RandomOrder.sellLimitUser2());
+    blackhole.consume(trades);
+    blackhole.consume(linkedListOrderBook.buildMarketData());
+  }
+
+  @Benchmark
+  public void measureMapOrderBook(Blackhole blackhole) {
+    mapOrderBook.add(RandomOrder.buyLimitUser1());
+    List<Trade> trades = mapOrderBook.match(RandomOrder.sellLimitUser2());
+    blackhole.consume(trades);
+    blackhole.consume(mapOrderBook.buildMarketData());
   }
 }
